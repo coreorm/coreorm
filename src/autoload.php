f5fa8f8f46da121cba3bc638a75f45e286451da3
Bruce <toughgamer@gmail.com>
2014-08-19 23:20:12 +1000

    remove unnecessary autoload. update composer (we don't need to load core here)

--- src/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * add autoload to project
- * @uses composer/autoload
- */
-require __DIR__ . '/../vendor/autoload.php';
-// core needs to be there
-require_once __DIR__ . '/Core.php';
-?>
