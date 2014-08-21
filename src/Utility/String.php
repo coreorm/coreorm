536f1e05c0479c617f5fe1d79db44cb55eadcaca
Bruce B Li <toughgamer@gmail.com>
2014-08-21 20:55:00 +1000

    add more string functions

--- src/Utility/String.php
+++ src/Utility/String.php
@@ -31,4 +31,39 @@ class String
 
     }// end camelCase
 
+
+    /**
+     * a little template engine, use :name to do the job
+     * @param $template
+     * @param $options
+     * @return mixed
+     */
+    public static function template($template, $options)
+    {
+        foreach ((array) $options as $key => $option) {
+            $template = str_replace(':' . $key, $option, $template);
+        }
+
+        return $template;
+
+    }// end template
+
+
+    /**
+     * shorten a string
+     * @param $string
+     * @param $len
+     * @param string $suffix
+     * @return string
+     */
+    public static function shorten($string, $len, $suffix = '...')
+    {
+        if ($len >= strlen($string)) {
+            return $string;
+        }
+
+        return substr($string, 0, $len) . $suffix;
+
+    }// end shorten
+
 }
